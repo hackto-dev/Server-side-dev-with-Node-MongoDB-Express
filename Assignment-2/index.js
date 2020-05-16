5f70ba9a4d5a650a4daa5e4d347f85a4ff5e9b19
hackto-dev <pratham.kulkarni1999@gmail.com>
2020-05-16 18:04:02 +0530

    Added Assignment 2

--- Assignment-2/index.js
+++ Assignment-2/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const dishRouter = require('./routes/dishRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
+const url = 'mongodb://localhost:27017/assignment';
+
+mongoose.connect(url, {
+    useMongoClient: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then((db) => {
+    console.log('Connected to database successfully');
+},(err) => {
+    console.log(err);
+});
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/leaders',leaderRouter);
+app.use('/dishes',dishRouter);
+app.use('/promotions',promotionRouter);
 
--- Assignment-2/models/promotions.model.js
+++ Assignment-2/models/promotions.model.js
@@ -1,3 +1,38 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
+
+const promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: mongoose.Types.Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const Promotions = mongoose.model('Promotion',promotionSchema);
+
+module.exports = Promotions;
